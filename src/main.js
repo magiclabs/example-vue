99009904b912f259f1047aabc401697448a2de34
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-02-16 23:33:28 +0530

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	created() {
+		const userString = localStorage.getItem('user')
+		if (userString) {
+			const userData = JSON.parse(userString)
+			this.$store.commit('SET_USER_DATA', userData)
+		}
+	},
+	render: (h) => h(App),
+}).$mount('#app')
